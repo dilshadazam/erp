23618dfdbbec1d96eda7e5016c77695645ee1d7b
dilshadazam <dilshadazam786@gmail.com>
2022-06-09 13:02:29 +0530

    setup completed with databse

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-// import path from "path";
+import path from "path";
 // import fs from "fs";
-// import cluster from "cluster";
-// import os from "os";
+import cluster from "cluster";
+import os from "os";
 import express from "express";
 // import multer from "multer";
 import sequelize from "./utilities/database.js";
 import helmet from "helmet";
 import compression from "compression";
 import dotenv from "dotenv";
-// import cors from "cors";
+import cors from "cors";
 
 //all error controllers imported here
-// import { corsError } from "./middleware/error-handlers/cors-error.js";
+import { corsError } from "./middleware/error-handlers/cors-error.js";
 
-// import { centralError } from "./middleware/error-handlers/central-errror.js";
+import { centralError } from "./middleware/error-handlers/central-errror.js";
 
 //all routes imported here
-// import authenticationRoutes from "./routes/authentication-routes.js";
-// import administratorRoutes from "./routes/administrator-routes.js";
-// import userRoutes from "./routes/user-routes.js";
-// import loanProvider from "./routes/loanprovider-routes.js";
+
 const cpu = os.cpus().length;
 
 if (process.env.NODE_ENV !== "production") {
@@ -31,24 +28,24 @@ const port = process.env.PORT || 3300;
 const app = express();
 
 //handle cors error
-// app.use(corsError);
+app.use(corsError);
 
-// app.use(cors());
-// app.options("*", cors());
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, X-Callback-Type, Content-Type, Accept"
-//   );
-//   res.header("Cache-Control", "no-cache");
-//   if ("OPTIONS" == req.method) {
-//     res.send(200);
-//   } else {
-//     next();
-//   }
-// });
+app.use(cors());
+app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, X-Callback-Type, Content-Type, Accept"
+  );
+  res.header("Cache-Control", "no-cache");
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+});
 
 if (cluster.isMaster) {
   console.log(`Master  ${process.pid} is running`);
@@ -80,7 +77,7 @@ if (cluster.isMaster) {
   //   },
   // });
   //defining absolute path of current WORKDIR
-  // const __dirname = path.resolve();
+  const __dirname = path.resolve();
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
@@ -108,23 +105,19 @@ if (cluster.isMaster) {
   //All routes entrypoint here
   // app.use("/auth", authenticationRoutes);
   // app.use("/administrator", administratorRoutes);
-  // app.use("/loanprovider", loanProvider);
-  // app.use("/user", userRoutes);
-  
 
   app.use(helmet());
   app.use(compression());
 
   //central error handler here
-  // app.use(centralError);
+  app.use(centralError);
 
   // sync with database
   sequelize
     .sync()
     .then(() => {
       app.listen(port);
-      console.log(
-  `✔️  Server listening on port: ${port}`);
+      console.log(`✔️  Server listening on port: ${port} ✔️ `);
     })
     .catch((err) => {
       console.log(err);
