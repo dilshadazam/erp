f9ef82466d8552432e0496b37bcd46b958606270
dilshadazam <dilshadazam786@gmail.com>
2022-06-17 18:05:59 +0530

    principal model

--- models/principal.js
+++ models/principal.js
@@ -1,76 +1,57 @@
-// import Sequelize from "sequelize";
+import Sequelize from "sequelize";
 
-// import sequelize from "../utilities/database.js";
-// import Admin from "./admin.js";
+import sequelize from "../utilities/database.js";
+import admin from "./admin.js";
 
-// const User = sequelize.define("user", {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     allowNull: false,
-//     primaryKey: true,
-//   },
-//   name: {
-//     type: Sequelize.STRING,
-//     allowNull: false,
-//   },
-//   email: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   password: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   phone: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   otp: {
-//     type: Sequelize.INTEGER,
-//     allowNull: true,
-//   },
-//   profileImageUrl: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-//   isAdmin: {
-//     type: Sequelize.BOOLEAN,
-//     defaultValue: false,
-//   },
-//   isPrincipal: {
-//     type: Sequelize.BOOLEAN,
-//     defaultValue: false,
-//   },
-//   isTeacher: {
-//     type: Sequelize.BOOLEAN,
-//     defaultValue: false,
-//   },
-//   isStudent: {
-//     type: Sequelize.BOOLEAN,
-//     defaultValue: false,
-//   },
-//   isAccountant: {
-//     type: Sequelize.BOOLEAN,
-//     defaultValue: false,
-//   },
+const Principal = sequelize.define("principal", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  adminId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    references: {
+      model: admin,
+      key: "id",
+    },
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+  profileImageUrl: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
 
-//   isAccountant: {
-//     type: Sequelize.BOOLEAN,
-//     defaultValue: false,
-//   },
-//   isAuthorized: {
-//     type: Sequelize.BOOLEAN,
-//     defaultValue: false,
-//   },
-//   isActive: {
-//     type: Sequelize.BOOLEAN,
-//     defaultValue: false,
-//   },
-//   refreshToken: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//   },
-// });
+  isPrincipal: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
 
-// export default User;
+  isAuthorized: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  isActive: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+  refreshToken: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+});
+
+export default Principal;
